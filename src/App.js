64bc5effb1af9eb1ccf86aacc86cf721d47335c7
myilmazz99 <46033929+myilmazz99@users.noreply.github.com>
2020-05-15 14:41:17 +0300

    added version view

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ export default class App extends Component {
   render() {
     return (
       <>
+        {/* v0.1 */}
         <Nav />
         <Home />
         {/* <Product /> deneme devam ediyor...*/}
