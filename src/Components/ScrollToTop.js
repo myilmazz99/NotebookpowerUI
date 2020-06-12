b0ddc919db5b6e01069d5da4542197ab5f396219
myilmazz99 <46033929+myilmazz99@users.noreply.github.com>
2020-06-13 00:53:17 +0300

    implemented querystring activation

--- src/Components/ScrollToTop.js
+++ src/Components/ScrollToTop.js
@@ -2,11 +2,11 @@ import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 
 const ScrollToTop = () => {
-  let { pathname } = useLocation();
+  let { pathname, search } = useLocation();
 
   useEffect(() => {
     window.scrollTo(0, 0);
-  }, [pathname]);
+  }, [pathname, search]);
 
   return null;
 };
