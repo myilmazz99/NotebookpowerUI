a0b3cf150f5f92dceda9f5feb95fe683d377cfd5
myilmazz99 <46033929+myilmazz99@users.noreply.github.com>
2020-10-07 04:00:01 +0300

    Updates

---
+++ precache-manifest.11d769aa77e776cd6c5723fc6c8f80d5.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f28ed2dc00de12bf1a925d9438ead04e",
+    "url": "/NotebookpowerUI/index.html"
+  },
+  {
+    "revision": "1d7cfe0c47ae55196baf",
+    "url": "/NotebookpowerUI/static/css/main.d42ba4c1.chunk.css"
+  },
+  {
+    "revision": "210dc5a36f1eb14ce921",
+    "url": "/NotebookpowerUI/static/js/0.0ee051c4.chunk.js"
+  },
+  {
+    "revision": "23f1420bf9d319c9bd85",
+    "url": "/NotebookpowerUI/static/js/10.dfd21d4e.chunk.js"
+  },
+  {
+    "revision": "d6e182d8e775a5099ac9",
+    "url": "/NotebookpowerUI/static/js/11.a7f72d69.chunk.js"
+  },
+  {
+    "revision": "81896c98bac7b5b16ab1d3790da5b937",
+    "url": "/NotebookpowerUI/static/js/11.a7f72d69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "269eb541446d42f00a8f",
+    "url": "/NotebookpowerUI/static/js/12.eed76b41.chunk.js"
+  },
+  {
+    "revision": "350aeba18ccd8b23b6aa",
+    "url": "/NotebookpowerUI/static/js/3.c130dc4b.chunk.js"
+  },
+  {
+    "revision": "9f90cde4396c52b73cc0a6bcd8267b7b",
+    "url": "/NotebookpowerUI/static/js/3.c130dc4b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9985c0c2bd67df9c3d9f",
+    "url": "/NotebookpowerUI/static/js/4.d48675c1.chunk.js"
+  },
+  {
+    "revision": "fbfc1952cdddbefe1601",
+    "url": "/NotebookpowerUI/static/js/5.86c9101f.chunk.js"
+  },
+  {
+    "revision": "82529fd6635d55720c90",
+    "url": "/NotebookpowerUI/static/js/6.85459006.chunk.js"
+  },
+  {
+    "revision": "7b717f08fbb9fb29f1d2",
+    "url": "/NotebookpowerUI/static/js/7.2ac02ad7.chunk.js"
+  },
+  {
+    "revision": "c1a0926dcf4351baf5ff",
+    "url": "/NotebookpowerUI/static/js/8.68c2cf5a.chunk.js"
+  },
+  {
+    "revision": "cf490ceec1ce92d9dbd1",
+    "url": "/NotebookpowerUI/static/js/9.d30195ee.chunk.js"
+  },
+  {
+    "revision": "1d7cfe0c47ae55196baf",
+    "url": "/NotebookpowerUI/static/js/main.878fb246.chunk.js"
+  },
+  {
+    "revision": "ab8a3880413b6848e968",
+    "url": "/NotebookpowerUI/static/js/runtime-main.d544559d.js"
+  },
+  {
+    "revision": "3f9e298816a709f0b91f6e931bc8e1f1",
+    "url": "/NotebookpowerUI/static/media/Code404.3f9e2988.svg"
+  },
+  {
+    "revision": "9cd0f06a687047f48d249218e5d35d97",
+    "url": "/NotebookpowerUI/static/media/accessoryBg.9cd0f06a.jpg"
+  },
+  {
+    "revision": "828f4e86eb587264a48de301b0a0854f",
+    "url": "/NotebookpowerUI/static/media/casualBg.828f4e86.jpg"
+  },
+  {
+    "revision": "1be6b7fbbad2147c0058666405adce31",
+    "url": "/NotebookpowerUI/static/media/category_accessories.1be6b7fb.jpg"
+  },
+  {
+    "revision": "1d18e0a75415a88313c3c85e2855eebc",
+    "url": "/NotebookpowerUI/static/media/category_casual.1d18e0a7.jpg"
+  },
+  {
+    "revision": "09ac2de57087a720d8e4ace7ecd3c30d",
+    "url": "/NotebookpowerUI/static/media/category_gaming.09ac2de5.jpg"
+  },
+  {
+    "revision": "535d7d7c74cee54156121635c3b52024",
+    "url": "/NotebookpowerUI/static/media/gamingBg.535d7d7c.png"
+  },
+  {
+    "revision": "36c05682d1030153982d5e0e2d4e8776",
+    "url": "/NotebookpowerUI/static/media/loadingSpinner.36c05682.gif"
+  },
+  {
+    "revision": "0f27625d9cd104a166009fe6b73c5ec1",
+    "url": "/NotebookpowerUI/static/media/logo.0f27625d.png"
+  },
+  {
+    "revision": "8a1ba30acb8b2a9397b7c10cd644e01b",
+    "url": "/NotebookpowerUI/static/media/promo1.8a1ba30a.png"
+  },
+  {
+    "revision": "ef516dff9095e5edd5190e722300265e",
+    "url": "/NotebookpowerUI/static/media/promo2.ef516dff.png"
+  },
+  {
+    "revision": "0ee52ce3c5b721183e0840f7b06892c4",
+    "url": "/NotebookpowerUI/static/media/promo3.0ee52ce3.png"
+  },
+  {
+    "revision": "ca5b131d374be2e44d8690e235616f88",
+    "url": "/NotebookpowerUI/static/media/safe-payment.ca5b131d.png"
+  }
+]);
