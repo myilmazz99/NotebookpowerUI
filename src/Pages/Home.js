8305b70d20fb4ce182d19f5a2472e8155c852676
myilmazz99 <46033929+myilmazz99@users.noreply.github.com>
2020-11-02 23:22:36 +0300

    removed slider and added banner

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,14 +1,14 @@
 import React from "react";
-import Slider from "../Components/Slider";
 import CategoryCards from "../Components/CategoryCards";
 import BestSelling from "../Components/Product/BestSelling";
 import DailyDeals from "../Components/Product/DailyDeals";
 import Mailing from "../Components/Mailing";
+import Banner from "../Components/Banner";
 
 const Home = () => {
   return (
     <main id="home">
-      <Slider />
+      <Banner />
       <CategoryCards />
       <BestSelling />
       <Mailing />
